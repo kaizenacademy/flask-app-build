018ecbb2b63a17f780e4e5d39d3fd5ba76465d0a
kaizenacademy <kaizen.techacademy@gmail.com>
2025-10-09 00:11:11 +0000

    added groovy script

---
+++ 1.groovy
@@ -0,0 +1,29 @@
+template = '''
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    run: docker
+  name: docker
+spec:
+  containers:
+  - command:
+    - sleep
+    - "3600"
+    image: docker
+    name: docker
+    '''
+
+podTemplate(cloud: 'kubernetes', label: 'docker', yaml: template) {
+node("docker") {
+    container("docker") {
+    stage("Checkout SCM") { 
+        git branch: 'main', url: 'https://github.com/kaizenacademy/flask-app-build.git'
+    } 
+
+    stage("Docker build") {
+        sh "docker build -t kaizenacademy/jenkins-flask-app:1.0.0 ."
+    }
+    }
+}
+}
